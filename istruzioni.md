0ac13393df737fd0279469fcf10bc97a933612d3
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-11-17 14:25:48 +0100

    update layout to display reviews

--- istruzioni.md
+++ istruzioni.md
@@ -20,12 +20,12 @@ La pagina di dettaglio di un singolo film ✅
 
 MILESTONE 3
 
-Configuriamo l’app di backend (repo webapp-express) a ricevere chiamate dalla nostra applicazione React, installando e impostando il middleware CORS
-Proviamo quindi ad effettuare una chiamata Ajax dalla home del progetto React, per ottenere la lista dei film
+Configuriamo l’app di backend (repo webapp-express) a ricevere chiamate dalla nostra applicazione React, installando e impostando il middleware CORS ✅
+Proviamo quindi ad effettuare una chiamata Ajax dalla home del progetto React, per ottenere la lista dei film ✅
 
 MILESTONE 4
 
-In ultimo, effettuiamo una chiamata AJAX dalla pagina di dettaglio per ottenere il dettaglio di un singolo film, comprese le sue recensioni
+In ultimo, effettuiamo una chiamata AJAX dalla pagina di dettaglio per ottenere il dettaglio di un singolo film, comprese le sue recensioni ✅
 
 Bonus
 Curare l’aspetto estetico della vostra applicazione
--- pages/MovieReview.jsx
+++ pages/MovieReview.jsx
@@ -1,62 +1,67 @@
 import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
+const API_URL = "http://localhost:3000/movies"
+
 
 export default function MovieReview() {
     const { id } = useParams();
+    const [movieReview, setMovieReview] = useState(null);
+
+    useEffect(() => {
+        axios.get(`${API_URL}/${id}`)
+            .then(res => {
+                console.log(res);
+                setMovieReview(res.data);
+            })
+            .catch(err => {
+                console.log(err.message)
+            });
+    }, [id])
 
     return (
         <>
             <div class="p-5 mb-4 bg-light rounded-3">
                 <div className="container-fluid d-flex gap-4 py-5">
                     <div className="cover col-12 col-sm-5 col-md-4">
-                        <img className="img-fluid " src="https://mr.comingsoon.it/imgdb/locandine/big/47397.jpg" alt="img" />
+                        <img className="img-fluid " src={movieReview?.image} alt={movieReview?.title} />
                     </div>
                     <div className="details">
-                        <h1 className="display-5 fw-bold">Titolo</h1>
+                        <h1 className="display-5 fw-bold">{movieReview?.title}</h1>
                         <p className="lead">
-                            Abstract: a jaded superhero (Ryan Reynolds) decides to embrace his
-                            supervillain side after a near-death experience and reunites with an old
-                            enemy (T.J. Miller) to form a team of ruthless mercenaries. But when
-                            the emergence of a new foe (Zazie Beetz) threatens to destroy
-                            everything, Deadpool must don his suit once again and save the world.
+                            {movieReview?.abstract}
                         </p>
-                        <span className="director d-block bi-person-circle"> Christopher Nolan </span>
-                        <span className="genre d-block bi-tag-fill"> Science Fiction </span>
-                        <span className="release_year d-block bi-calendar-fill"> 2010 </span>
+                        <span className="director d-block bi-person-circle"> {movieReview?.director} </span>
+                        <span className="genre d-block bi-tag-fill"> {movieReview?.genre} </span>
+                        <span className="release_year d-block bi-calendar-fill"> {movieReview?.release_year} </span>
                     </div>
 
 
                 </div>
             </div>
 
+            {/* reviews list */}
             <section className="reviews">
-                <div className="container mb-4">
-                    <div className="list-group">
-                        <a href="#" className="list-group-item list-group-item-action" aria-current="true">
-                            <div className="d-flex w-100 justify-content-between">
-                                <h5 className="mb-1">Name</h5>
-                            </div>
-                            <p className="mb-1">Some placeholder content in a paragraph.</p>
-                            <small className="bi-star-fill">Vote</small>
-                        </a>
-                        <a href="#" className="list-group-item list-group-item-action" aria-current="true">
-                            <div className="d-flex w-100 justify-content-between">
-                                <h5 className="mb-1">Name</h5>
-                            </div>
-                            <p className="mb-1">Some placeholder content in a paragraph.</p>
-                            <small className="bi-star-fill">Vote</small>
-                        </a>
-                        <a href="#" className="list-group-item list-group-item-action" aria-current="true">
-                            <div className="d-flex w-100 justify-content-between">
-                                <h5 className="mb-1">Name</h5>
-                            </div>
-                            <p className="mb-1">Some placeholder content in a paragraph.</p>
-                            <small className="bi-star-fill">Vote</small>
-                        </a>
 
-                    </div>
+                <div className="container mb-4">
+                    {movieReview?.reviews?.map(review => {
+                        return (
+                            <div className="list-group mb-2" key={review.id}>
+                                <a href="#" className="list-group-item list-group-item-action" aria-current="true">
+                                    <div className="d-flex w-100 justify-content-between">
+                                        <h5 className="mb-1">{review.name}</h5>
+                                    </div>
+                                    <p className="mb-1">{review.text}</p>
+                                    <small className="bi-star-fill me-2">{review.vote}</small>
+                                </a>
+                            </div>)
+
+                    })}
+
                 </div>
             </section>
 
+            {/* review form */}
             <div className="container">
                 <section className="write_review mb-4">
                     <h3>Lascia la tua recensione</h3>
